37cc696eda4af073b4ec8c81a5868b8586a8e021
RupinderN <rupinder0410@gmail.com>
2019-09-07 03:21:33 +0000

    first commit

--- app.js
+++ app.js
@@ -3,9 +3,55 @@
 // ==============
 const express = require('express'),
 	  app = express(),
-	  request = require("request"),
-	  mongoose = require('mongoose'),
-	  bodyParser = require('body-parser'),
-	  imdb = require('imdb'),
-	  rp = require('request-promise');
+	  http = require("http"),
+	  SpotifyWebApi = require("spotify-web-api-node");
 
+app.set('view engine', 'ejs');
+
+// ==============
+// CONFIGURATION
+// ==============
+var spotifyApi = new SpotifyWebApi({
+	scopes: ['user-read-private', 'user-read-email'],
+	redirectUri: 'https://webprojects-rqwyg.run.goorm.io/callback/',
+	clientSecret: 'CLIENT_SECRET',
+	clientId: 'CLIENT_ID',
+	state: "STATE"
+});
+
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(spotifyApi._credentials.scopes, spotifyApi._credentials.state);
+
+// =======
+// ROUTES
+// =======
+app.get('/login', function(req, res){
+	res.redirect(authorizeURL);
+});
+
+app.get('/callback', function(req, res) {
+	var code = req.query.code;
+	
+	spotifyApi.authorizationCodeGrant(code).then(function(data) {
+		// Set the access token on the API object to use it in later calls
+		spotifyApi.setAccessToken(data.body['access_token']);
+		spotifyApi.setRefreshToken(data.body['refresh_token']);
+		res.redirect('/main');
+	}, function(err) {
+		console.log('Something went wrong!', err);
+	});
+});
+
+app.get('/main', function(req, res) {
+	spotifyApi.getArtistRelatedArtists('7rkW85dBwwrJtlHRDkJDAC').then(function(data) {
+		console.log(data.body.artists);
+	}, function(err) {
+		done(err);
+	});
+});
+
+
+app.listen(3000, (req, res) => {
+	console.log("Server is Running...");
+});
